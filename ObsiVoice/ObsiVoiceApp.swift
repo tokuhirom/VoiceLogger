23313a7116de0a198b951f29b31187e174307851
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-06-27 22:19:57 +0900

    Initial Commit

---
+++ ObsiVoice/ObsiVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObsiVoiceApp.swift
+//  ObsiVoice
+//
+//  Created by 松野 徳大 on 2025/06/27.
+//
+
+import SwiftUI
+
+@main
+struct ObsiVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObsiVoiceTests/ObsiVoiceTests.swift
@@ -0,0 +1,16 @@
+//
+//  ObsiVoiceTests.swift
+//  ObsiVoiceTests
+//
+//  Created by 松野 徳大 on 2025/06/27.
+//
+
+import Testing
+
+struct ObsiVoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
